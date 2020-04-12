e9fefe8f86131a592cbfcb6bb5382becd5aa5340
AlexanderM <alexmak@findo.com>
2020-04-12 20:26:37 +0300

    remove warnings from redis

--- shared/bash.sh
+++ shared/bash.sh
@@ -13,7 +13,6 @@ HISTSIZE=10000
 HISTFILESIZE=10000
 shopt -s histappend # Append history instead of rewriting it
 shopt -s cmdhist # Use one command per line
-export PS1="[\\[\\033[38;5;75m\\]\\h\\[\\033[0m\\]][\\w]\\$ "
 
 _set_access_group 'default'
 
@@ -25,7 +24,7 @@ _projects_bash()
     cur_word="${COMP_WORDS[COMP_CWORD]}"
     prev_word="${COMP_WORDS[COMP_CWORD-1]}"
 
-    projects_list=$(redis-cli -a "${ISOLATE_REDIS_PASS}" get "projects_list" | tr '[:upper:]' '[:lower:]'  2>>/dev/null)
+    projects_list=$(redis-cli --no-auth-warning -a "${ISOLATE_REDIS_PASS}" get "projects_list" | tr '[:upper:]' '[:lower:]'  2>>/dev/null)
 
     if [ "${COMP_CWORD}" -eq 1 ]; then
         COMPREPLY=( $(compgen -W "${projects_list}" -- "${cur_word}") )
@@ -46,9 +45,9 @@ _project_host_bash()
     cur_word="${COMP_WORDS[COMP_CWORD],,}"
     prev_word="${COMP_WORDS[COMP_CWORD-1],,}"
 
-    projects_list=$(redis-cli -a "${ISOLATE_REDIS_PASS}" get "projects_list" | tr '[:upper:]' '[:lower:]'  2>>/dev/null)
+    projects_list=$(redis-cli --no-auth-warning -a "${ISOLATE_REDIS_PASS}" get "projects_list" | tr '[:upper:]' '[:lower:]'  2>>/dev/null)
 
-    hosts_list=$(redis-cli -a "${ISOLATE_REDIS_PASS}" get "complete_hosts_${prev_word}" | tr '[:upper:]' '[:lower:]'  2>>/dev/null )
+    hosts_list=$(redis-cli --no-auth-warning -a "${ISOLATE_REDIS_PASS}" get "complete_hosts_${prev_word}" | tr '[:upper:]' '[:lower:]'  2>>/dev/null )
 
     if [ "${COMP_CWORD}" -eq 1 ]; then
         COMPREPLY=( $(compgen -W "${projects_list}" -- "${cur_word}") )
